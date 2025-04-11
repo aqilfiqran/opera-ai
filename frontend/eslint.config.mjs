cfcea385d88d0ece7d8bb1f0308d4210cd49437b
aqilfiqran <aqilfiqran19@gmail.com>
2025-04-11 08:35:27 +0700

    chore: frontend - setup eslint config

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,6 +1,8 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
+import sonarjs from 'eslint-plugin-sonarjs';
+import security from 'eslint-plugin-security';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,7 +12,78 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends(
+    'next/core-web-vitals',
+    'next/typescript',
+    'prettier',
+    'plugin:jsx-a11y/recommended',
+    'plugin:react-hooks/recommended'
+  ),
+  {
+    files: ['*.ts', '*.tsx'],
+    languageOptions: {
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        tsconfigRootDir: __dirname,
+        project: './tsconfig.json',
+      },
+    },
+    plugins: {
+      sonarjs,
+      security,
+    },
+    rules: {
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          args: 'all',
+          argsIgnorePattern: '^_',
+          caughtErrors: 'all',
+          caughtErrorsIgnorePattern: '^_',
+          destructuredArrayIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          ignoreRestSiblings: true,
+        },
+      ],
+      '@typescript-eslint/no-explicit-any': 'error',
+      'react/react-in-jsx-scope': 'off',
+      'react/jsx-filename-extension': [
+        1,
+        {
+          extensions: ['.ts', '.tsx', '.js', '.jsx'],
+        },
+      ],
+      'react/jsx-props-no-spreading': 'off',
+      'import/extensions': [
+        'error',
+        'ignorePackages',
+        {
+          js: 'never',
+          jsx: 'never',
+          ts: 'never',
+          tsx: 'never',
+        },
+      ],
+      'jsx-a11y/anchor-is-valid': [
+        'error',
+        {
+          components: ['Link'],
+          specialLink: ['hrefLeft', 'hrefRight'],
+          aspects: ['invalidHref', 'preferButton'],
+        },
+      ],
+      'no-nested-ternary': 'off',
+      'import/prefer-default-export': 'off',
+      'import/no-extraneous-dependencies': 'off',
+      'react/require-default-props': 'off',
+      'react/no-array-index-key': 'off',
+      'jsx-a11y/click-events-have-key-events': 'off',
+      'security/detect-object-injection': 'off',
+      'sonarjs/no-nested-template-literals': 'off',
+      'jsx-a11y/no-static-element-interactions': 'off',
+      'import/no-unresolved': 'error',
+    },
+  },
 ];
 
 export default eslintConfig;
